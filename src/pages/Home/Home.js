1905f7e168d6b60ac8502d8a9de45acca10c88d9
Sean Dylan Patterson <sean@seanpatterson.co.za>
2023-04-14 20:04:15 +0200

    components

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,30 @@
-import { Typography } from '@mui/material'
 import Page from 'material-ui-shell/lib/containers/Page'
 import React from 'react'
 import { useIntl } from 'react-intl'
+import AppBar from 'pages/appBar'
+import MediaControlCard from 'pages/mediaControl'
+import RecipeReviewCard from 'pages/complexCard'
+import ComplexGrid from 'pages/complexGrid'
+import SpeedDial from 'pages/speedDial'
+import ScrollableTabs from 'pages/scrollableTabs'
+// import PaginationLink from 'pages/routedPagination'
+import BottomNavigation from 'pages/bottomNav'
+import SpacingGrid from 'pages/grid'
 
 const HomePage = () => {
   const intl = useIntl()
 
   return (
     <Page pageTitle={intl.formatMessage({ id: 'home' })}>
-      <Typography>{intl.formatMessage({ id: 'home' })}</Typography>
+      <AppBar />
+      <ScrollableTabs />
+      <MediaControlCard />
+      {/* <PaginationLink /> */}
+      <ComplexGrid />
+      <SpacingGrid />
+      <RecipeReviewCard />
+      <SpeedDial />
+      <BottomNavigation />
     </Page>
   )
 }
